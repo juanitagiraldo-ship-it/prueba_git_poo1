986f98815eb5425d86707a1e40b0953335096f82
juanitagiraldo-ship-it <juanita.giraldo@autonoma.edu.co>
2026-06-12 15:34:11 -0500

    Intento de error

--- src/main/java/org/example/Busqueda.java
+++ src/main/java/org/example/Busqueda.java
@@ -2,15 +2,29 @@ package org.example;
 
 public class Busqueda {
 
-    public int buscar(int[] lista, int valor) {
-        int bajo = 0;
-        int alto = lista.length - 1;
-        while (bajo <= alto){
-            int medio = bajo + (alto - bajo) / 2;
-            if (lista[medio] == valor) return medio;
-            if (lista[medio] < valor) bajo = medio + 1;
-            else alto = medio - 1;
+    public void sort(int[] arr) {
+        int n = arr.length;
+        boolean huboIntercambio;
+
+        for (int i = 0; i < n - 1; i++) {
+            huboIntercambio = false;
+
+            // El último elemento i ya está ordenado, no hace falta evaluarlo
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar arr[j] y arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    huboIntercambio = true;
+                }
+            }
+
+            // Si no hubo intercambios en esta pasada, el arreglo ya está ordenado
+            if (!huboIntercambio) {
+                break;
+            }
         }
-        return -1; 
+    }
     }
 }
